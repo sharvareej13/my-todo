189de89f60bf1e4b4bc72cfe80df511fe4fed699
Jadhav <sjad@softwareag.com>
2021-06-28 00:11:23 +0530

    changes2

--- configuration/databased.js
+++ configuration/databased.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const config = require('../config');
+const config = require('./config');
 
 
 const { dBB: { usernamedb, password, server, database } } = config;
