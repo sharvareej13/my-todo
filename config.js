f2962abebb7eb074c68b986908e717f760132aa3
Jadhav <sjad@softwareag.com>
2021-06-28 00:01:20 +0530

    with config

---
+++ config.js
@@ -0,0 +1,11 @@
+const config = {
+    dBB:{
+        usernamedb: 'shara',
+        password: 'sharamongo%4013',
+        server: 'cluster0.z7wpn.mongodb.net',
+        database: 'myFirstDatabase'
+    }
+
+};
+
+module.exports = config;
